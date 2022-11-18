056e155eae14c531d320c6aaeab76f74adcc5d0c
dancasnar1 <dancasnar1@alum.us.es>
2022-11-18 11:18:55 +0100

    FizzBuzz done

---
+++ src/FizzBuzz/FizzBuzzV2.java
@@ -0,0 +1,25 @@
+package FizzBuzz;
+
+import java.util.Scanner;
+
+public class FizzBuzzV2 {
+	public static void main(String args[])   {
+		Scanner sc = new Scanner(System.in);  
+		System.out.print("Enter the number: ");  
+		int n = sc.nextInt();
+		if (n%3==0) {
+			System.out.print("fizz ");  
+		} 
+		if (n%5==0) {
+			System.out.print("buzz ");  
+		}
+		if (n%7==0) {
+			System.out.print("pop ");  
+		}
+		if (n%3 != 0 && n%5 != 0 && n%7 != 0) {
+			System.out.print(n);
+		}
+		sc.close();
+	}
+}   
+
